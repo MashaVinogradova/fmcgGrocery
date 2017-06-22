710470c9c8f6e98e8c7163eb9ff886e8c2acdb48
mohitagarwal <mohit.tibrewal@flipkart.com>
2017-06-22 17:19:43 +0530

    initial commit

---
+++ app/src/main/java/com/flipkart/fmcggrocery/fmcggrocery/BarCodeScannerActivity.java
@@ -0,0 +1,19 @@
+package com.flipkart.fmcggrocery.fmcggrocery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class BarCodeScannerActivity extends AppCompatActivity{
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+}
